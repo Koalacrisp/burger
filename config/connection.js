5b711e4d27d46c978f961978db7ad61216542ad2
Koalacrisp <finnpdolson@gmail.com>
2017-08-12 02:22:31 -0400

    Added Heroku support

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,18 @@ var mysql = require("mysql");
 
 // Sets up the connection to the Burger database
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "SQLmy123",
-  database: "burgers_db"
-});
+var connection
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost'
+		user: 'root',
+		password: 'SQLmy123',
+		database: 'burgers_db'
+	});
+}
 
 connection.connect(function(err) {
   if (err) {
